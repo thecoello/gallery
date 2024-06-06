e54500e027bc764345273c82ec9928d568506d9a
Manuel Alejandro Guerra Coello <thecoello@gmail.com>
2024-06-06 16:01:15 +0200

    *

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react"
-import projectsLocal from "./assets/projects";
-import { gsap, Power2, Expo, Linear} from "gsap";
+import { gsap, Power2, Expo, Linear } from "gsap";
 
 export default class App extends React.Component {
 
@@ -8,55 +7,70 @@ export default class App extends React.Component {
     super(props);
     this.state = {
       projectsArr: [],
+      _imagesLoaded: false,
+      dataProjects: []
     }
   }
 
   componentDidMount() {
-    this.projects()
+    this.getData()
+  }
+
+  getData(){
+    fetch('./src/assets/projects.JSON')
+    .then(res => res.json())
+    .then(data => {
+      this.setState({dataProjects: data})
+    })
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
-    if (prevState.projectsArr.length != this.state.projectsArr.length) {
+    if(prevState.dataProjects != this.state.dataProjects){
+      this.projects()
+    }
+    if(prevState.projectsArr.length != this.state.projectsArr.length){
       this.loadImages()
+    }
+    if(prevState._imagesLoaded != this.state._imagesLoaded){
+      this.animationStart()
     }
   }
 
-  loadImages=()=>{
+  loadImages() {
     const images = document.querySelectorAll('img')
     const imagesAvailable = images.length
     let imagesLoaded = 0
     let loaded = 0
-    let tl = gsap.timeline()
 
     images.forEach(img => {
       img.addEventListener('load', (e) => {
         imagesLoaded = imagesLoaded + 1
         loaded = 100 / imagesAvailable * imagesLoaded
-        gsap.to('.loader', 4, { width: `${loaded}%`, ease: Power2.easeOut, overwrite: true})
+        gsap.to('.loader', 4, { width: `${loaded}%`, ease: Power2.easeOut, overwrite: true })
+
         if(loaded == 100){
-          tl.set('#brand-name-wrap',{alpha: 1})
-          tl.fromTo('#brand-name', 1,{yPercent: 100},{ yPercent: 0, ease: Expo.easeOut, delay: 4.5})
-          tl.fromTo('.intro', 1,{height: '100vh'},{ height: '25vh', ease: Expo.easeOut })
-          tl.to('#brand-name-wrap',1,{top: '0vh', delay: -1, ease: Expo.easeOut})
-          tl.to('.loader-container', 1,{top: '25vh', bottom: '0vh', delay: -1, transformOrigin: 'right', ease: Expo.easeOut, onStart: ()=>{
-            let counter = 0
-            document.querySelectorAll('.project').forEach(project => {
-              gsap.fromTo(project,1,{y: 20, alpha: 0},{y: 0, delay: counter, alpha: 1, ease: Expo.easeInOut})
-              counter += 0.1
-            })
-          } })
+          this.setState({ _imagesLoaded: true })
         }
 
-      })  
-    
+      })
     })
   }
 
-  projects=()=> {
+  animationStart() {
+    let tl = new gsap.timeline()
+    tl.set('#brand-name-wrap', { alpha: 1 })
+    tl.to('#brand-name', 1, { y: `${0}vh`, ease: Expo.easeOut, delay: 4.5 })
+    tl.fromTo('.intro', 1, { height: `${100}vh` }, { height: `${25}vh`, ease: Expo.easeOut })
+    tl.to('#brand-name-wrap', 1, { y: 0, delay: -1, ease: Expo.easeOut })
+    tl.to('.loader-container', 1, {y: `${15}vh`, delay: -1, transformOrigin: 'right', ease: Expo.easeOut})
+    tl.fromTo('.project', 1,{ y: 10, alpha: 0},{ y: 0, alpha: 1, ease: Expo.easeInOut, stagger: 0.1, delay: -1.5})
+  }
+
+  projects = () => {
     let projectsArr = []
-    projectsLocal.forEach((project, i) => {
+    this.state.dataProjects.forEach((project, i) => {
       projectsArr.push(<div className="project" key={`${i}-project`}>
-         <div className="project-image">
+        <div className="project-image">
           <img src={project.image} alt={project.nombre} />
         </div>
         <div className="project-text-wrap">
@@ -65,7 +79,7 @@ export default class App extends React.Component {
         </div>
         <div className="border-top"></div>
 
-       
+
       </div>)
     });
     this.setState({ projectsArr: projectsArr })
@@ -77,7 +91,7 @@ export default class App extends React.Component {
 
         <div className="intro">
           <div id="brand-name-wrap">
-          <h3 id="brand-name">Gallery</h3>
+            <h3 id="brand-name">Gallery</h3>
           </div>
           <div className="loader-container">
             <div className="loader">
